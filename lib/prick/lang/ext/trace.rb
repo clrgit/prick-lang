a488bcf630e6803ffe55455ec43cd122856f56a7
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2025-09-20 00:49:15 +0200

    Reimplemented Trace

--- lib/prick/lang/ext/trace.rb
+++ lib/prick/lang/ext/trace.rb
@@ -1,8 +1,42 @@
 require 'indented_io'
 
-
-# Currently used to avoid polluting Kernel with helper methods
 module Trace
+  @@TRACERS = {}
+  @@TRACER_COUNTS = {}
+  @@ENABLED = false
+
+  def self.enabled? = @@ENABLED
+  def self.enable() @@ENABLED = true end
+  def self.disable() @@ENABLED = false end
+
+  def self.key(klass, method) = "#{klass}##{method}"
+
+  def self.create_tracer(key)
+    @@TRACER_COUNTS[key] = 0
+    @@TRACERS[key] = TracePoint.new(:return) { |tp|
+      key = "#{tp.defined_class}##{tp.method_id}"
+      @@TRACER_COUNTS[key] -= 1
+      if @@TRACER_COUNTS[key] == 0
+        @@TRACERS[key].disable # self.disable ?
+      end
+      Kernel.undent
+    }
+  end
+
+  def self.ensure_tracer(key)
+    create_tracer(key) if ! @@TRACERS.key?(key)
+  end
+
+  def self.enable_tracer(key, klass, method)
+    @@TRACERS[key].enable(target: klass.instance_method(method)) if @@TRACER_COUNTS[key] == 0
+    @@TRACER_COUNTS[key] += 1
+  end
+
+  def self.disable_tracer(key)
+    @@TRACER_COUNTS[key] -= 1
+    @@TRACERS[key].disable if @@TRACER_COUNTS[key] == 0
+  end
+
   def self.render_arg(arg)
     arg.is_a?(Hash) ? arg.to_s[1..-2] : arg.inspect
   end
@@ -12,59 +46,25 @@ module Trace
       ""
     else
       values = (args.map(&:inspect) + [opts.empty? ? nil : opts.to_s[1..-2]])
-      "(" + values.compact.join(", ") + ")"
+      "(" + values.flatten.compact.join(", ") + ")"
     end
   end
 end
 
-trace = TracePoint(:return) { |tp|
-  puts "Method: #{tp.method_id}"
-}
-trace.enable
-
 module Kernel
-  @@TRACES = []
-
   def trace(*args, **opts)
-    stack = caller.map { |l| l =~ /^.*:.*:in `(\w+)'$/; $1 }.compact
-    function = stack.first
+    return if !Trace.enabled?
+    stack = caller.map { |l| l =~ /^.*:.*:in `(\S+)'$/; $1 }
+    name = stack.first
+    method = stack.first
+    klass = self.class
 
-#   if function == "analyze_provide"
-#     $stderr.puts "-----------------------------------"
-#     $stderr.puts "> stack"
-#     $stderr.indent { |f| f.puts stack }
-#     $stderr.puts "> function: #{function.inspect}"
-#     $stderr.puts "> @@TRACES: #{@@TRACES.inspect}"
-#   end
+    key = "#{klass}##{method}"
+    Trace.ensure_tracer(key)
+    Trace.enable_tracer(key, klass, method)
 
-    if @@TRACES.first == function
-      @@TRACES.shift
-      undent
-    end
-
-    traces = []
-    trace_i = 0
-    for entry in stack
-      if entry == @@TRACES[trace_i]
-        traces << entry
-        trace_i += 1
-      end
-    end
-
-    undent_levels = @@TRACES.size - traces.size
-    @@TRACES = [function] + traces
-#   if function == "analyze_provide"
-#     $stderr.puts "> @@TRACES: #{@@TRACES.inspect}"
-#   end
-    undent_levels.times { Kernel.undent }
-
-    puts "##{function}#{Trace.render_args *args, **opts}"
+    puts "##{name}#{Trace.render_args *args, **opts}"
     Kernel.indent
-  end
-
-  def retrace
-    @@TRACES.size.times { Kernel.undent }
-    @@TRACES = []
   end
 end
 
